cb667419e70f4feffeadc751463689c1f4156b03
rafael-veiga <rafaelvalenteveiga@gmail.com>
2025-04-19 16:11:05 +0100

    small change

--- start.sh
+++ start.sh
@@ -16,14 +16,14 @@ fi
 
 # build docker images
 docker build -f dockers/compile1docker/Dockerfile -t compile1docker .
-docker run -it --rm -v "$(pwd)/c_src/comp1":/app compile1docker /bin/bash -c "g++ -std=c++17 -o3 read_csv.cpp Data.cpp List.cpp -o process_file"
+docker run --rm -v "$(pwd)/c_src/comp1":/app compile1docker /bin/bash -c "g++ -std=c++17 -o3 read_csv.cpp Data.cpp List.cpp -o process_file"
 mv "$(pwd)/c_src/comp1/process_file" "$(pwd)/scripts"
 chmod +x "$(pwd)/scripts/process_file"
 
 docker build -f dockers/compile2docker/Dockerfile -t compile2docker .
-docker run -it --rm -v "$(pwd)/c_src/comp2":/app compile2docker /bin/bash -c "c++ -O3 -Wall -Wextra -shared -std=c++17 -fPIC $(python3 -m pybind11 --includes) c_py.cpp -o pyfileconverter$(python3-config --extension-suffix)"
-mv "$(pwd)/c_src/comp2/pyfileconverter.*" "$(pwd)/scripts"
-chmod +x "$(pwd)/scripts/pyfileconverter.*"
+docker run --rm -v "$(pwd)/c_src/comp2":/app compile2docker bash -c 'c++ -O3 -Wall -Wextra -shared -std=c++17 -fPIC $(python3 -m pybind11 --includes) c_py.cpp -o pyfileconverter$(python3-config --extension-suffix)'
+mv "$(pwd)"/c_src/comp2/pyfileconverter.* "$(pwd)/scripts"
+chmod +x "$(pwd)"/scripts/pyfileconverter.*
 
 docker build -f dockers/pre1docker/Dockerfile -t pre1docker .
 nextflow run main.nf -resume $NEXTFLOW_ARGS
